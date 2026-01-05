9f3bc142ef2679c508559f6caa15d87200ea09b7
Raul Lucas <raullinjetmc@gmail.com>
2026-01-05 17:02:23 -0600

    initial commit

---
+++ include/AudioEngine.hpp
@@ -0,0 +1,14 @@
+#pragma once
+#include <SFML/Audio.hpp>
+#include <vector>
+#include <string>
+#ifndef AUDIO_ENGINE_HPP
+#define AUDIO_ENGINE_HPP
+
+class AudioEngine {
+public:
+
+private:
+};
+
+#endif
