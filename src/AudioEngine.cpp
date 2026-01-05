09f7db5236a669dc4f02ee54b85eef83de1633d2
Raul Lucas <raullinjetmc@gmail.com>
2026-01-04 19:09:06 -0600

    added play and loadfile methods

--- src/AudioEngine.cpp
+++ src/AudioEngine.cpp
@@ -6,3 +6,19 @@
 AudioEngine::AudioEngine() {
 }
 
+AudioEngine::~AudioEngine() {
+}
+
+void AudioEngine::play() {
+    sound.play();
+}
+// -------------------------------
+bool AudioEngine::loadFile(const std::string& filePath) {
+    if (!buffer.loadFromFile(filePath)) {
+        std::cerr << "Error: Could not open file " << filePath << std::endl;
+        return false;
+    }
+    sound.setBuffer(buffer);
+    return true;
+}
+
