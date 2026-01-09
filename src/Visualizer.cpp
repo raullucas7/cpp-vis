ae3ec8c2754a7a00b3726ee79bd040c71f415911
Raul Lucas <raullinjetmc@gmail.com>
2026-01-09 15:52:18 -0600

    initial commit + added methods

--- src/Visualizer.cpp
+++ src/Visualizer.cpp
@@ -1,7 +1,50 @@
 #include "Visualizer.hpp"
+#include <iostream>
 
-Visualizer::Visualizer() {
+Visualizer::Visualizer() 
+{
+    windowsize = 1024;
+    waveform.setPrimitiveType(sf::LineStrip);
+    waveform.resize(windowsize);
 }
 
-Visualizer::~Visualizer() {
+Visualizer::~Visualizer() 
+{
+}
+
+void Visualizer::run() 
+{
+    window.create(sf::VideoMode(800, 600), "CPP-Vis");
+    window.setFramerateLimit(60);
+
+    if (!AudioEngine.loadFile(filePath)) 
+    {
+        return;
+    }
+
+    AudioEngine.play();
+
+    while (window.isOpen())
+    {
+        EventProcess();
+        update();
+        render();
+    }
+}
+
+void Visualizer::render() 
+{
+    window.create(800, 600);
+    window.setFramerateLimit(60);
+    
+}
+
+void::update() 
+{
+
+}
+
+void::EventProcess()
+{
+    
 }
