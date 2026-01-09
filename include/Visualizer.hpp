6fbbf8ddab4b7ee6f2af45c2d8b480a921a7c418
Raul Lucas <raullinjetmc@gmail.com>
2026-01-08 20:41:55 -0600

    completed visualizer engine

--- include/Visualizer.hpp
+++ include/Visualizer.hpp
@@ -1,10 +1,27 @@
 #ifndef VISUALIZER_HPP
 #define VISUALIZER_HPP
+#pragma once
+#include <SFML/Graphics.hpp>
+#include "AudioEngine.hpp"
 
 class Visualizer {
 public:
     Visualizer();
     ~Visualizer();
+    AudioEngine();
+
+    void run(const std::string& filePath);
+    void render();
+    void EventProcess();
+    void update();
+
+private:
+    sf::RenderWindow window;
+    sf::VertexArray waveform;
+
+    // engine instance
+    AudioEngine audioEngine;
+    std::size_t samplecount;
 };
 
 #endif
